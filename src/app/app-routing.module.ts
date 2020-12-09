306af3410d8ee05025e3f781f9d1f6e7d945c0f7
Faith Olusegun <GD0024@fidelitybank.ng>
2020-12-09 21:23:41 +0100

    First Build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component' ;
+import { ShopComponent } from './shop/shop.component';
+import { BlogComponent } from './blog/blog.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'about', component: AboutComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
